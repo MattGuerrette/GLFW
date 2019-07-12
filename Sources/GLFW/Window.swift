b1cbf7e6b65dd04768db54cec8ee26063e31a314
MattGuerrette <direct3Dtutorials@gmail.com>
2019-07-12 00:12:35 -0400

    test

--- Sources/GLFW/Window.swift
+++ Sources/GLFW/Window.swift
@@ -65,7 +65,7 @@ public class Window {
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
 
         
         opaque = glfwCreateWindow(Int32(width), Int32(height), title, monitor?.opaque, nil)
