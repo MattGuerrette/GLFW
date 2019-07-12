6a71715a7bc04679c3744180a811c3de273d6e80
MattGuerrette <direct3Dtutorials@gmail.com>
2019-07-12 00:17:52 -0400

    core profile

--- Sources/GLFW/Window.swift
+++ Sources/GLFW/Window.swift
@@ -64,13 +64,13 @@ public class Window {
         
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
 
         
         opaque = glfwCreateWindow(Int32(width), Int32(height), title, monitor?.opaque, nil)
         glfwMakeContextCurrent(opaque);
-        //glfwSwapInterval(1);
+        glfwSwapInterval(1);
         
         glfwSetWindowUserPointer(opaque, Unmanaged.passUnretained(self).toOpaque())
     }
